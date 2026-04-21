ac14fd1ef7ff45515f7eb75a07148c7db1ef2f9a
Martin Schulz <martinschulzofficial@gmail.com>
2026-04-22 01:01:37 +0500

    add icons

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,66 @@
 #include "widget.h"
 #include "./ui_widget.h"
+#include <QSystemTrayIcon>
+#include <QMenu>
+
+#include <QAction>
+#include <QCheckBox>
+#include <QComboBox>
+#include <QCoreApplication>
+#include <QCloseEvent>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QMenu>
+#include <QPushButton>
+#include <QSpinBox>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QMessageBox>
+
+const int kTrayIconOn = 1;
+const int kTrayIconOff = 0;
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    createIconGroupBox();
+    createTrayIcon();
+
+    // setIcon(kTrayIconOff);
+    setIcon(kTrayIconOn);
+    trayIcon->show();
+}
+
+void Widget::setIcon(int index)
+{
+    QIcon icon = iconComboBox->itemIcon(index);
+    trayIcon->setIcon(icon);
+    setWindowIcon(icon);
+
+    trayIcon->setToolTip(iconComboBox->itemText(index));
+}
+
+void Widget::createIconGroupBox()
+{
+    iconGroupBox = new QGroupBox(tr("Tray Icon"));
+
+    iconComboBox = new QComboBox;
+    iconComboBox->addItem(QIcon(":/images/zzz-off.svg"), tr("Off"));
+    iconComboBox->addItem(QIcon(":/images/zzz.svg"), tr("On"));
+
+    QHBoxLayout *iconLayout = new QHBoxLayout;
+    iconLayout->addWidget(iconComboBox);
+    iconLayout->addStretch();
+    iconGroupBox->setLayout(iconLayout);
+}
+
+void Widget::createTrayIcon()
+{
+    trayIcon = new QSystemTrayIcon(this);
 }
 
 Widget::~Widget()
--- widget.h
+++ widget.h
@@ -2,11 +2,14 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QSystemTrayIcon>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Widget;
 }
+class QComboBox;
+class QGroupBox;
 QT_END_NAMESPACE
 
 class Widget : public QWidget
@@ -17,7 +20,18 @@ public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget() override;
 
+
+private slots:
+    void setIcon(int index);
+
 private:
     Ui::Widget *ui;
+    void createIconGroupBox();
+    void createTrayIcon();
+
+    QSystemTrayIcon *trayIcon;
+
+    QComboBox *iconComboBox;
+    QGroupBox *iconGroupBox;
 };
 #endif // WIDGET_H
