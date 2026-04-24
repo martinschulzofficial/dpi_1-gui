f1f0f82c5b0f6add2f45103cc6f09f90778bdf37
Martin Schulz <martinschulzofficial@gmail.com>
2026-04-24 17:43:16 +0500

    show form

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
-    // w.show();
+    w.show();
     a.setQuitOnLastWindowClosed(false);
     return QCoreApplication::exec();
 }
